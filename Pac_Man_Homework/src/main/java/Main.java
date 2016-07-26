490749329ecdd59db5bef3762446466bdf51d82d
yixing <yixing@ccs.neu.edu>
2016-07-26 11:10:22 -0400

    update homework 3 excutor

--- Pac_Man_Homework/src/main/java/Main.java
+++ Pac_Man_Homework/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import entrants.pacman.yixing.*;
 import examples.commGhosts.POCommGhosts;
 import pacman.Executor;
+import pacman.controllers.examples.StarterGhosts;
 
 
 /**
@@ -16,9 +17,13 @@ public class Main {
 
         // HW2 implemented uniformed search and informed search
         // uninformed search
-        executor.runGameTimed(new MyPacMan_BFS(), new POCommGhosts(50), true);
-        executor.runGameTimed(new MyPacMan_DLS(), new POCommGhosts(50), true);
+//        executor.runGameTimed(new MyPacMan_BFS(), new POCommGhosts(50), true);
+//        executor.runGameTimed(new MyPacMan_DLS(), new POCommGhosts(50), true);
         // informed search
 //        executor.runGameTimed(new MyPacMan_AStar(), new POCommGhosts(50), true);
+//        executor.runGameTimed(new MyPacMan_GBFS(), new POCommGhosts(50), true);
+        // HW3 implemented adevarsrial search
+        // alpha beta pruning
+        executor.runGameTimed(new MyPacMan_AlphaBetaPurning(), new POCommGhosts(50), true);
     }
 }
